757df168e339f9f3866ed582b12ca27e22ee4986
evertonnatan <evertonnatandl@outlook.com>
2021-03-26 20:26:06 -0300

    Instalando o nodemon

--- src/server.js
+++ src/server.js
@@ -1,2 +1,10 @@
 const express = require('express')
+const server = express()
+
+server.get('/', (request, response) => {
+    return response.send("Oie!!")
+})
+
+server.listen(3000, () => console.log("Server running on port 3000"))
+
 
