11dd99c9bea990bed508bb2b72d5a615bc79079a
evertonnatan <evertonnatandl@outlook.com>
2021-03-27 19:45:37 -0300

    Aula 01 finalizada

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
+const profile = {
+    name: "Everton",
+    avatar: "https://avatars.githubusercontent.com/u/37844708?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
 
-routes.get('', (req, res) => res.render(views + "index"))
-routes.get('job', (req, res) => res.render(views + "job"))
-routes.get('jobedit', (req, res) => res.render(views + "job-edit"))
-routes.get('profile', (req, res) => res.render(views + "profile"))
+
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile } ))
 
 module.exports = routes
